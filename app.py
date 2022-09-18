e3757571cf32e9545f723e40345a710cebfeb07c
Anmol Jaiswal <68013660+anmolg1997@users.noreply.github.com>
2022-09-18 14:40:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+
 import plotly.express as px
 from datetime import datetime 
 from pil import Image
@@ -8,3 +9,4 @@ from pil import Image
 st.title("Birthday Celebration")
 today = datetime.now().strftime("%M%d, %Y")
 st.markdown("%s", today)
+st.markdown("AMAR MANI TRIPATHI")
